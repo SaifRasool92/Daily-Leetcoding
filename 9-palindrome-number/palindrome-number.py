81f640eb3f03a5d34ede106d7240b13b1539fb58
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-01-10 10:26:51 +0500

    Time: 2 ms (90.76%) | Memory: 17.7 MB (34.35%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -3,5 +3,6 @@ class Solution:
         x_str = str(x)
         reversed_str = ""
         for char in x_str:
+    #here the sequence of 'char' and 'reverse_str' matters
             reversed_str = char + reversed_str
         return reversed_str == x_str
