7c3afbe944a4ee15790afaccefba2badced14275
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-01-12 20:37:41 +0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (21.96%) - LeetSync

--- 1848-sum-of-unique-elements/sum-of-unique-elements.py
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.py
@@ -1,6 +1,8 @@
 class Solution:
     def sumOfUnique(self, nums: List[int]) -> int:
-        
-        from collections import Counter
-        counts = Counter(nums)
-        return sum(num for num, count in counts.items() if count == 1)
+        total = 0
+        for num in nums:
+            if nums.count(num) == 1:
+                total += num
+        return total
+
