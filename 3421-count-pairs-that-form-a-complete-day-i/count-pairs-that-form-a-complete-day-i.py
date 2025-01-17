fc8bcdf9340260ddd1fb7ea93333b84d1a2e28dd
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-01-17 12:18:23 +0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (46.33%) - LeetSync

--- 3421-count-pairs-that-form-a-complete-day-i/count-pairs-that-form-a-complete-day-i.py
+++ 3421-count-pairs-that-form-a-complete-day-i/count-pairs-that-form-a-complete-day-i.py
@@ -1,9 +1,19 @@
 class Solution:
     def countCompleteDayPairs(self, hours: List[int]) -> int:
-        n = len(hours)
+        # n = len(hours)
+        # count = 0
+        # for i in range(n):
+        #     for j in range(i+1, n):
+        #         if (hours[i] + hours[j]) % 24 == 0:
+        #             count +=1
+        # return count
+        mod_count = defaultdict(int)  # To store the count of each remainder modulo 24
         count = 0
-        for i in range(n):
-            for j in range(i+1, n):
-                if (hours[i] + hours[j]) % 24 == 0:
-                    count +=1
+
+        for hour in hours:
+            remainder = hour % 24
+            complement = (24 - remainder) % 24  # Find the complement modulo 24
+            count += mod_count[complement]  # Add the pairs that form a complete day
+            mod_count[remainder] += 1  # Update the count of the current remainder
+
         return count
