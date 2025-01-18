9d9256cc4cd25d98bc80494360a23946a814fc0e
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-01-18 18:21:14 +0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (14.46%) - LeetSync

---
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.py
@@ -0,0 +1,9 @@
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+
+       empty_set = set()
+       for i in s:
+        if i in empty_set:
+            return i
+
+        empty_set.add(i)
