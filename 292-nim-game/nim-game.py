48146aee94300e6f72d4901b72d588cb81f875dd
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-01-22 12:13:55 +0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (53.93%) - LeetSync

---
+++ 292-nim-game/nim-game.py
@@ -0,0 +1,7 @@
+class Solution:
+    def canWinNim(self, n: int) -> bool:
+        for i in range(n+1):
+            if n % 4 == 0:
+                return False
+            else:
+                return True
