c39841f37314f4b86ef2dd5bb4ca957b8fc82610
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-03-04 10:59:51 +0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (98.58%) - LeetSync

--- 2886-faulty-keyboard/faulty-keyboard.py
+++ 2886-faulty-keyboard/faulty-keyboard.py
@@ -1,10 +1,10 @@
 class Solution:
     def finalString(self, s: str) -> str:
-        result = []
-
-        for char in s:
-            if char == "i":
+        result = [] 
+        for char in s:  #s = "string"
+            if char == 'i':
                 result.reverse()
             else:
                 result.append(char)
+        
         return ''.join(result)
