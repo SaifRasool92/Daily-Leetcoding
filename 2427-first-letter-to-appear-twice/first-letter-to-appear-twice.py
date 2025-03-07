29cd51d8adfef7c3e65684660dd1c8e99a825cac
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-03-07 10:19:03 +0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (26.21%) - LeetSync

--- 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.py
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.py
@@ -1,9 +1,9 @@
 class Solution:
     def repeatedCharacter(self, s: str) -> str:
 
-       empty_set = set()
-       for i in s:
-        if i in empty_set:
-            return i
+        empty_set = set()  # {'a','b','c','d}
+        for i in s:             # s = "abcdd"
+            if i in empty_set:
+                return i
 
-        empty_set.add(i)
+            empty_set.add(i)
