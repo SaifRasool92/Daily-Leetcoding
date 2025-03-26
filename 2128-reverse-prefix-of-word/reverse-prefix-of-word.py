7820b0b7b233f33841863b8ae22f48279796e9c9
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-03-26 12:30:24 +0500

    Time: 0 ms (100.00%) | Memory: 18 MB (11.68%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
@@ -1,20 +1,39 @@
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
-        word_list = list(word)
+        if ch not in word:
+            return word  # If ch is not found, return the word as is
+
+        index = word.index(ch)  # Find the first occurrence of ch
+        prefix_reversed = word[:index + 1][::-1]  # Reverse the prefix including ch
+        return prefix_reversed + word[index + 1:]
+#         word_list = list(word)  #word = "abcdefd", ch = "d" Output: "dcbaefd"  ["a", "b", "c", "d", "e", "f", "h"]
         
-        index = -1
-        for i in range(len(word_list)):
-            if word_list[i] == ch:
-                index = i
-                break
+#         index = -1
+#         for i in range(len(word_list)):
+#             if word_list[i] == ch: 
+#                 index = i
+#                 break
         
-        if index == -1:
-            return word
+#         if index == -1:
+#             return word
         
-        left, right = 0, index
-        while left < right:
-            word_list[left], word_list[right] = word_list[right], word_list[left]
-            left += 1
-            right -= 1
+#         left, right = 0, index
+#         while left < right:
+#             word_list[left], word_list[right] = word_list[right], word_list[left]
+#             left += 1
+#             right -= 1
         
-        return "".join(word_list)
+#         return "".join(word_list)
+
+# class Solution:
+#     def reversePrefix(self, word: str, ch: str) -> str:
+#         output = []
+#         if ch not in word:
+#             return word
+#         for i in word:
+#             if i == ch:
+#                 output.append(ch)
+#                 output.reverse()
+#             else:
+#                 output.append(i)
+#         return ''.join(output)
