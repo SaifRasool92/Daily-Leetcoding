215b13ba08c2f8d502c5820c3df1fa09b222531f
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-04-18 18:34:09 +0500

    Time: 3 ms (15.25%) | Memory: 18 MB (44.63%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.py
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -3,13 +3,18 @@ class Solution:
         # ans =  nums + nums
         # return ans
         # Input: nums = [1,2,1]
-        # Output: [1,2,1,1,2,1]
+        # Output: [1,2,1,1,2,1] = 2n
 
+        # ans = []
+
+        # for j in range(2):
+        #     for i in nums:
+        #         ans.append(i)
+        # return ans
+
+        length = len(nums) * 2 # lenghth = 6
         ans = []
-
-        for j in range(2):
-            for i in nums:
-                ans.append(i)
+        for i in range(length): # 1 = 0, 1 ,2 ,3 
+            ans.append(nums [i % len(nums)]) # len(nums) = 3 i % len(nums)
 
         return ans
-
