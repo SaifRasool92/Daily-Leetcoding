3ea633cb05369e4b091444d88d335cbd206f922c
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-05-20 20:14:45 +0500

    Time: 0 ms (100.00%) | Memory: 18.1 MB (10.19%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        for char in s:
+            if char == '(':
+                stack.append(')')
+
+            elif char == '[':
+                stack.append(']')
+
+            elif char == '{':
+                stack.append('}')
+
+            else:
+
+                if len(stack) == 0:
+                    return False  # no matching opening bracket
+                top = stack.pop()
+                if char != top:
+                    return False  # mismatched bracket
+
+        return len(stack) == 0  # true if all matched     
