617b1a00bb63954607c88baa29bb5aae223bbdc7
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-05-22 15:29:42 +0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (52.70%) - LeetSync

--- 1505-create-target-array-in-the-given-order/create-target-array-in-the-given-order.py
+++ 1505-create-target-array-in-the-given-order/create-target-array-in-the-given-order.py
@@ -3,7 +3,10 @@ class Solution:
         target = []
 
         for i in range(len(nums)):
-            target.insert(index[i], nums[i] )
+            target.insert(index[i], nums[i])
 
         return target
             
+
+# Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
+# Output: [0,4,1,3,2]
