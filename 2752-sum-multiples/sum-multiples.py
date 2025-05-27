c0d007dee19f1ad0b33107df439547f9d266d0d0
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-05-27 18:58:19 +0500

    Time: 28 ms (37.27%) | Memory: 17.9 MB (23.60%) - LeetSync

--- 2752-sum-multiples/sum-multiples.py
+++ 2752-sum-multiples/sum-multiples.py
@@ -1,18 +1,29 @@
+
 class Solution:
     def sumOfMultiples(self, n: int) -> int:
-        nums = []
-        for i in range(1, n+1):
-            nums.append(i)
+        total = 0
+        for i in range(1, n + 1):  
+            if i % 3 == 0:
+                total += i
+            elif i % 5 == 0:
+                total += i
+            elif i % 7 == 0:     
+                total += i
+        return total
 
-        total_sum = 0
+        # nums = []
+        # for i in range(1, n+1):
+        #     nums.append(i)
 
-        for num in nums:
-            if num % 3 == 0:
-                total_sum += num
-            elif num % 5 == 0:
-                total_sum += num
-            elif num % 7 == 0:
-                total_sum += num
+        # total_sum = 0
 
-        return total_sum
+        # for num in nums:
+        #     if num % 3 == 0:
+        #         total_sum += num
+        #     elif num % 5 == 0:
+        #         total_sum += num
+        #     elif num % 7 == 0:
+        #         total_sum += num
+
+        # return total_sum
             
