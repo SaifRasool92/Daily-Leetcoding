e9a30a7633e0caab7205e741faf177254269ec0e
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-05-31 12:42:02 +0500

    Time: 8 ms (66.14%) | Memory: 17.8 MB (51.72%) - LeetSync

--- 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.py
+++ 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.py
@@ -11,13 +11,15 @@ class Solution:
 
         if len(nums) < 3:
             return -1  
+        nums.remove(max(nums))
+        nums.remove(min(nums))
 
-        copy_nums = nums[:] 
-        copy_nums.remove(max(copy_nums))  
-        copy_nums.remove(min(copy_nums))  
+        # copy_nums = nums[:] 
+        # copy_nums.remove(max(copy_nums))  
+        # copy_nums.remove(min(copy_nums))  
 
-        if copy_nums:
-            return copy_nums.pop() 
+        if nums:
+            return nums.pop() 
         return -1
 
      
