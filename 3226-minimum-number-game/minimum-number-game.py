d8bcec3f70439dd2871db01fe811f1d02e7eda79
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-06-10 19:10:46 +0500

    Time: 0 ms (100.00%) | Memory: 18 MB (15.44%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.py
+++ 3226-minimum-number-game/minimum-number-game.py
@@ -1,8 +1,17 @@
 class Solution:
     def numberGame(self, nums: List[int]) -> List[int]:
-        n = len(nums)
         nums.sort()
-        for i in range(0, n, 2):
-            if i + 1 < n:
-                nums[i], nums[i + 1] = nums[i + 1], nums[i]
-        return nums
+        arr = []
+        for i in range(0, len(nums), 2):
+            alice = nums[i]
+            bob = nums[i + 1]
+            arr.append(bob)   # Bob appends first
+            arr.append(alice) # Then Alice appends
+        return arr
+
+        # n = len(nums)
+        # nums.sort()
+        # for i in range(0, n, 2):
+        #     if i + 1 < n:
+        #         nums[i], nums[i + 1] = nums[i + 1], nums[i]
+        # return nums
