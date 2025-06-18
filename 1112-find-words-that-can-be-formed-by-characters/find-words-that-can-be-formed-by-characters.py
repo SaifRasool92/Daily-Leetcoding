0480aa835710b4f460c167e2f03ddeeb4f6de685
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-06-18 11:59:48 +0500

    Time: 72 ms (58.83%) | Memory: 18.5 MB (27.13%) - LeetSync

--- 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.py
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.py
@@ -1,5 +1,33 @@
 class Solution:
     def countCharacters(self, words: List[str], chars: str) -> int:
+        # chars_dict = {}
+        # for ch in chars:
+        #     if ch in chars_dict:
+        #         chars_dict[ch] += 1
+        #     else:
+        #         chars_dict[ch] = 1
+
+        # total_length = 0
+
+        # for word in words:
+        #     word_dict = {}
+        #     for ch in word:
+        #         if ch in word_dict:
+        #             word_dict[ch] += 1
+        #         else:
+        #             word_dict[ch] = 1
+
+        #     can_make = True
+        #     for ch in word:
+        #         if ch not in chars_dict or word_dict[ch] > chars_dict[ch]:
+        #             can_make = False
+        #             break
+
+        #     if can_make:
+        #         total_length += len(word)
+
+        # return total_length
+
         chars_dict = {}
         for ch in chars:
             if ch in chars_dict:
@@ -8,18 +36,17 @@ class Solution:
                 chars_dict[ch] = 1
 
         total_length = 0
-
         for word in words:
-            word_dict = {}
+            words_dict = {}
             for ch in word:
-                if ch in word_dict:
-                    word_dict[ch] += 1
+                if ch in words_dict:
+                    words_dict[ch] += 1
                 else:
-                    word_dict[ch] = 1
+                    words_dict[ch] = 1
 
             can_make = True
             for ch in word:
-                if ch not in chars_dict or word_dict[ch] > chars_dict[ch]:
+                if ch not in chars_dict or words_dict[ch] > chars_dict[ch]:
                     can_make = False
                     break
 
