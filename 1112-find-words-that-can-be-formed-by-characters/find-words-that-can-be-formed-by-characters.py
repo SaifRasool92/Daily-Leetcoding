cc05c2ea13c5d79db466d14337b55a3c7f5f1228
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-06-18 12:01:53 +0500

    Time: 75 ms (55.05%) | Memory: 18.2 MB (73.62%) - LeetSync

--- 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.py
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.py
@@ -1,33 +1,5 @@
 class Solution:
     def countCharacters(self, words: List[str], chars: str) -> int:
-        # chars_dict = {}
-        # for ch in chars:
-        #     if ch in chars_dict:
-        #         chars_dict[ch] += 1
-        #     else:
-        #         chars_dict[ch] = 1
-
-        # total_length = 0
-
-        # for word in words:
-        #     word_dict = {}
-        #     for ch in word:
-        #         if ch in word_dict:
-        #             word_dict[ch] += 1
-        #         else:
-        #             word_dict[ch] = 1
-
-        #     can_make = True
-        #     for ch in word:
-        #         if ch not in chars_dict or word_dict[ch] > chars_dict[ch]:
-        #             can_make = False
-        #             break
-
-        #     if can_make:
-        #         total_length += len(word)
-
-        # return total_length
-
         chars_dict = {}
         for ch in chars:
             if ch in chars_dict:
@@ -36,17 +8,18 @@ class Solution:
                 chars_dict[ch] = 1
 
         total_length = 0
+
         for word in words:
-            words_dict = {}
+            word_dict = {}
             for ch in word:
-                if ch in words_dict:
-                    words_dict[ch] += 1
+                if ch in word_dict:
+                    word_dict[ch] += 1
                 else:
-                    words_dict[ch] = 1
+                    word_dict[ch] = 1
 
             can_make = True
             for ch in word:
-                if ch not in chars_dict or words_dict[ch] > chars_dict[ch]:
+                if ch not in chars_dict or word_dict[ch] > chars_dict[ch]:
                     can_make = False
                     break
 
@@ -54,3 +27,4 @@ class Solution:
                 total_length += len(word)
 
         return total_length
+
