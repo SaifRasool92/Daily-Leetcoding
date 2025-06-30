ba1a5821ea5136c83971dc968a9cb1e46f50bf9f
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-06-30 12:08:55 +0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (71.08%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -22,3 +22,9 @@ class Solution:
         return result
 
 
+# 3 Method
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return list(set(nums1)&set(nums2))
+
+
