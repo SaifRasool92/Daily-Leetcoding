5439daeb07672b79a94776fddce5be389626b9ae
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-07-13 18:24:10 +0500

    Time: 15 ms (18.24%) | Memory: 17.7 MB (64.37%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,5 +1,25 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        x = str(x)
+        empty = []
+        for num in x:
+            empty.append(num)
+
+        l = 0
+        r = len(empty) - 1
+        while l < r:
+            empty[l], empty[r] = empty[r], empty[l]
+            l += 1
+            r -= 1
+        empty = ''.join(empty)
+
+        return int(empty) == int(x)
+
+        """2nd Method"""
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
         x_str = str(x)
         reversed_str = ""
         for char in x_str:
