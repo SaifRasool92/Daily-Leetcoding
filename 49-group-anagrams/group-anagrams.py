7e58a8c200f61c1a2751af72d49675bb8cefe906
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-07-17 20:03:11 +0500

    Time: 11 ms (82.75%) | Memory: 20.5 MB (95.70%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,10 +1,16 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        group = {}
+
+        group = {}   #  strs = ["eat","tea","tan","ate","nat","bat"]
+
         for i in strs:
             key = "".join(sorted(i))
             if key in group:
                 group[key].append(i)
             else:
                 group[key] = [i]
+
         return list(group.values())
+
+
+        # group = {'aet': ['eat','tea', 'ate'], 'ant':['tan','nat'], 'abt':'bat'}
