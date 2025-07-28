6470199a4088864e9d6f4baf417678ab20f571df
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-07-28 08:43:29 +0500

    Time: 0 ms (100.00%) | Memory: 18.1 MB (23.50%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
@@ -1,7 +1,39 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
+        temp = 0
+        for i in range(len(nums)):
+            temp = temp + nums[i]
+            nums[i] = temp
+        return nums
+
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
         prev = 0
         for i in range(len(nums)):
             nums[i] = nums[i] + prev
             prev = nums[i]
+        return nums
+
+#Brute Force:
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        result = []
+        for i in range(len(nums)):
+            result.append(sum(nums[:i+1]))
+        return result
+# Optimezed Solution
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        sum = 0
+        for i in range(len(nums)):
+            sum += nums[i]
+            nums[i] = sum
+        return nums
+
+# More Optimized
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        for i in range(1,len(nums)):
+            nums[i] += nums[i-1]
         return nums
