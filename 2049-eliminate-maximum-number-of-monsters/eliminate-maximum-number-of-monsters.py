1cd6a95dc2b8402fabe293ff9eeffd0777ef12f8
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-07-30 20:23:57 +0500

    Time: 60 ms (86.27%) | Memory: 32.4 MB (34.15%) - LeetSync

---
+++ 2049-eliminate-maximum-number-of-monsters/eliminate-maximum-number-of-monsters.py
@@ -0,0 +1,15 @@
+# from math import ceil
+
+class Solution:
+    def eliminateMaximum(self, dist: List[int], speed: List[int]) -> int:
+        steps = []
+        for i in range(len(dist)):
+            steps.append(ceil(dist[i] / speed[i]))
+        
+        steps.sort()
+        res = 0
+        for monster_time in steps:
+            if res >= monster_time:
+                return res
+            res += 1
+        return res
