421bf0553a712c3f2480fa0c037d09ab1d2a45aa
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-08-05 21:37:09 +0500

    Time: 3 ms (50.48%) | Memory: 19.1 MB (10.20%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -4,5 +4,29 @@ class Solution:
             for j in range(i + 1, len(nums)) :
                 if nums[i] + nums[j] == target:
                     return [i,j]
-                
-                
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pair_idx = {}
+
+        for i, num in enumerate(nums):
+            if target - num in pair_idx:
+                return [i, pair_idx[target - num]]
+            pair_idx[num] = i
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numMap = {}
+        n = len(nums)
+
+        # Build the hash table
+        for i in range(n):
+            numMap[nums[i]] = i
+
+        # Find the complement
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in numMap and numMap[complement] != i:
+                return [i, numMap[complement]]
+
+        return []  # No solution found
