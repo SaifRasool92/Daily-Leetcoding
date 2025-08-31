77fab338d947401370ab763b9cf780cdb511dd0c
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-08-31 21:54:17 +0500

    Time: 0 ms (100.00%) | Memory: 18.5 MB (28.60%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.py
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.py
@@ -1,18 +1,3 @@
-# class Solution:
-#     def reverseWords(self, s: str) -> str:
-#         ans = []
-#         s = s.split()
-#         for char in s:  
-#             l = 0
-#             r = len(char) -1
-#             while l < r:
-#                 char[l], char[r] = char[r], char[l]
-#                 l += 1
-#                 r -= 1
-#             ans.append(char)
-
-#         return ' '.join(ans)
-
 class Solution:
     def reverseWords(self, s: str) -> str:
         ans = []
@@ -27,3 +12,17 @@ class Solution:
                 r -= 1
             ans.append(''.join(char))   # convert list back to string
         return ' '.join(ans)
+        
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words = s.split() # list
+
+        for i in range(len(words)):
+            word = words[i]
+            reversed_word = word[::-1] # slicing
+            words[i] = reversed_word
+
+        # form string from list
+        ans = " ".join(words)
+
+        return ans
