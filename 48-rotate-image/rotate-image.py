4edbcda38bbbbff5be6ad2f28e74bd8d0f46c08b
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-09-08 20:58:16 +0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (22.37%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -3,10 +3,23 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        for i in range(len(matrix)):
-            for j in range(i+1, len(matrix[0])):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        #  matrix = [[1,2,3], -> [3,2,1]
+        #            [4,5,6], -> [6,5,4]
+        #            [7,8,9]] -> [9,8,7]
         
-        for i in range(len(matrix)):
+        for i in range(len(matrix)):  # -> 3
+            for j in range(i+1, len(matrix[0])): # -> 1,2
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+                
+                # matrix[0][1], matrix[1][0] = matrix[1][0], matrix[0][1]
+                # matrix[0][2], matrix[2][0] = matrix[2][0], matrix[0][2]
+                
+                # matrix[1][2], matrix[2][1] = matrix[2][1], matrix[1][2]
+        
+        for i in range(len(matrix)): #4
             matrix[i].reverse()
         
+                
+               #  matrix = [[1,2,3], | [[1,4,7],
+        #                   [4,5,6], |  [2,5,8],
+        #                   [7,8,9]] |  [3,6,9]]
