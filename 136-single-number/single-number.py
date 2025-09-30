53afb45417f38f7f00cbdc986c2bfc1f0647b0af
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-09-30 10:50:57 +0500

    Time: 0 ms (100.00%) | Memory: 19.7 MB (42.39%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        dic = {}
+        for i in nums:  #nums = [4,1,2,1,2]
+
+            if i in dic:
+                dic[i] += 1
+            else:
+                dic[i] = 1
+                
+        for i in dic:
+            if dic[i] == 1:
+                return i
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        xor = 0
+        for val in nums:
+            xor ^= val
+        return xor
