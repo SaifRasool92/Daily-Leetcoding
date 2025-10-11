5decda8341da9bd6e5970bded5aac2d62a79c043
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-10-11 17:16:21 +0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (18.66%) - LeetSync

--- 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.py
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.py
@@ -18,3 +18,9 @@ class Solution:
                 ans.append(w)
         
         return ans
+
+
+class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+        count = Counter((s1 + " " + s2).split())
+        return [word for word, freq in count.items() if freq == 1]
