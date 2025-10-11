5b2efbf603d087dd2e735e7af559d63aa5d4e7fa
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-10-11 20:45:41 +0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (38.87%) - LeetSync

--- 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.py
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.py
@@ -1,26 +1,25 @@
 class Solution:
     def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
-        s1 = s1.split()
-        s2 = s2.split()
-
-        words = s1 + s2 #->u248pl
+        """
+        Input: s1 = "this apple is sweet", s2 = "this apple is sour"
+        words = this apple is sweet this apple is sour
         
-        dic = {}   
+        count = {'this':2, 'apple':2, 'is':2, 'sweet':1, 'sour':1}
+        """
+        
+        words = (s1 + " "+ s2).split()
+        count = {}
+        
         for w in words:
-            if w in dic:
-                dic[w] += 1 
+            if w in count:
+                count[w] += 1
             else:
-                dic[w] = 1
-        
+                count[w] = 1
+       
         ans = []
-        for w in dic:
-            if dic[w] == 1:
-                ans.append(w)
+        for key in count:
+            if count[key] == 1:
+                ans.append(key)
         
         return ans
-
-
-class Solution:
-    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
-        count = Counter((s1 + " " + s2).split())  #-> u786il
-        return [word for word, freq in count.items() if freq == 1]  #->u894ip
+        
