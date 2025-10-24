d26f3bbd14737ef252ebccf640f0f229dcdf807f
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-10-24 17:22:31 +0500

    Time: 0 ms (100.00%) | Memory: 18.2 MB (18.09%) - LeetSync

--- 682-baseball-game/baseball-game.py
+++ 682-baseball-game/baseball-game.py
@@ -1,14 +1,15 @@
 class Solution:
     def calPoints(self, operations: List[str]) -> int:
-        res = []
-        for i in range(len(operations)):
-            try:
-                res.append(int(operations[i]))
-            except ValueError:
-                if operations[i] == "+":
-                    res.append(res[-2] + res[-1])
-                elif operations[i] == "D":
-                    res.append(res[-1] * 2)
-                elif operations[i] == "C":
-                    res.pop()  # pop is way quicker here than del
-        return sum(res)
+        stack = []
+        
+        for op in operations:
+            if op == "C":
+                stack.pop()
+            elif op == "D":
+                stack.append(stack[-1] * 2)
+            elif op == "+":
+                stack.append(stack[-1] + stack[-2])
+            else:
+                stack.append(int(op))
+        
+        return sum(stack)
