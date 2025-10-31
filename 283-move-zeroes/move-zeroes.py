cffb15c90ec396d6c32be18461a676afac3ea79c
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-10-31 23:49:26 +0500

    Time: 9834 ms (5.00%) | Memory: 19.1 MB (9.78%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -5,6 +5,17 @@ class Solution:
         """
         n = len(nums)
         for i in range(n):
-            for j in range(0, n - i - 1):
-                if nums[j] == 0 and nums[j + 1] != 0:
-                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+            for j in range(n-i-1):
+                if nums[j] == 0 and nums[j+1] != 0:
+                    nums[j], nums[j+1] = nums[j+1], nums[j]
+
+# class Solution:
+#     def moveZeroes(self, nums: List[int]) -> None:
+#         """
+#         Do not return anything, modify nums in-place instead.
+#         """
+#         n = len(nums)
+#         for i in range(n):
+#             for j in range(0, n - i - 1):
+#                 if nums[j] == 0 and nums[j + 1] != 0:
+#                     nums[j], nums[j + 1] = nums[j + 1], nums[j]
