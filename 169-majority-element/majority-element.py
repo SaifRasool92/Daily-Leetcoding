ef068585681ea266cb8db803a12e4d2657167517
Saif Ur Rasool <rasoolsaif24@gmail.com>
2026-03-01 12:36:04 +0500

    Time: 11 ms (32.51%) | Memory: 21.2 MB (44.04%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,24 +1,60 @@
 class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        
-        
-        ans = 0
-        n = len(nums)
-        maj =  n // 2
-        freq = {}
-        
-        # nums = [2,2,1,1,1,2,2]
-        for num in nums:
-            # if num in freq:
-            #     freq[num] += 1
-            # else:
-            #     freq[num] = 1
-            freq[num] = freq.get(num, 0) + 1  # Update frequency
-             
-            if freq[num] > maj:
-                ans = num
+    def majorityElement(self, nums: List[int]) -> int:   
+        # Phase 1
+        majority = nums[0] 
+        count = 1    
+        for i in range(1,len(nums)):
+            if nums[i] == majority:
+                count += 1
+            else:
+                count -= 1
+                if count == 0:
+                    majority = nums[i]
+                    count = 1  
+                    
+        # Phase 2
+        count = 0   
+        for i in range(len(nums)):
+            if nums[i] == majority:
+                count +=1
                 
-        return ans
+                
+        if count > len(nums) // 2:
+            return majority
+
+        
+
+
+
+
+
+
+
+
+
+
+# """ Better Approch """
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> int:
+        
+        
+#         ans = 0
+#         n = len(nums)
+#         maj =  n // 2
+#         freq = {}
+        
+#         # nums = [2,2,1,1,1,2,2]
+#         for num in nums:
+#             # if num in freq:
+#             #     freq[num] += 1
+#             # else:
+#             #     freq[num] = 1
+#             freq[num] = freq.get(num, 0) + 1  # Update frequency
+             
+#             if freq[num] > maj:
+#                 ans = num
+                
+#         return ans
         
         
         
@@ -27,29 +63,10 @@ class Solution:
         
         
      
+
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-# """Naive Approch"""
-                            
-                            
+# """Naive Approch"""                         
 # class Solution:
 #     def majorityElement(self, nums: List[int]) -> int:
 #     majority = -1
