30316c6c9c180c0960304bdec3f083a92ea582f5
Saif Ur Rasool <rasoolsaif24@gmail.com>
2026-03-04 10:25:56 +0500

    Time: 367 ms (7.42%) | Memory: 19.8 MB (40.35%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
@@ -1,23 +1,43 @@
 class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
-        n = len(grid) ** 2
+        def find_missing(grid):
+            n = len(grid) ** 2
+            elements = []
+            for nums in grid:
+                for num in nums:
+                    elements.append(num)
+
+            unique = set(elements)
+            total_sum = n * (n + 1) // 2
+            grid_sum = sum(unique)
+            missing = total_sum - grid_sum
+            return missing
+
+        def find_repeat(elements):
+            """This will find repeat element"""
+            repeat = 0
+            for num in elements:
+                if elements.count(num) > 1:
+                    repeat = num
+                    break
+            return repeat
+
+        # Create elements list once
         elements = []
         for nums in grid:
             for num in nums:
                 elements.append(num)
-        unique = set(elements)
-        
-        total_sum = n * (n+1) // 2
-        grid_sum  = sum(unique)
-        
-        missing = total_sum - grid_sum
-        repeat = 0
-        for num in elements:
-            if elements.count(num) > 1:
-                repeat = num
-                break
-        
+
+        repeat = find_repeat(elements)
+        missing = find_missing(grid)
+
         return [repeat, missing]
 
                 
-                
+# class Solution:
+#     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        
+        
+        
+# class Solution:
+#     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:                     
